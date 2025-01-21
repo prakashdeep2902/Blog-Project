5b3f9b4126401ea164452730b14b1b607ef0a451
sharmaJi <prakashdeep2902@gmail.com>
2025-01-21 07:17:47 +0530

    added navbar in project

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import express from "express";
+import path from "path";
 const app = express();
 const PORT = 8000;
 
 app.set("view engine", "ejs");
-app.set("views", "views");
+app.set("views", path.resolve("./views"));
 
 app.get("/", (req, res) => {
   res.render("home");
